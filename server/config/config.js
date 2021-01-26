0480a4ca8bb1af9e7a812dfa049734c15a70c874
Saw Luke Loo Wah <lukieloowah009@gmail.com>
2021-01-25 20:17:19 -0500

    Created config.js

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://gatorlorian:60t2b@geo.5fotp.mongodb.net/Geo?retryWrites=true&w=majority', //place the URI of your mongo database here.
+    }
+};
