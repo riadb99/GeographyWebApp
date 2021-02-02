8de508fc67416ad96de178fd30ad0f574e25f011
Riad Becirevic <rbecirevic@ufl.edu>
2021-02-02 17:19:14 -0500

    Initialized Map Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home";
 import NotFound from "./views/NotFound";
 import NavBar from "./components/Header/NavBar";
+import ReactTooltip from "react-tooltip";
+import "./components/Map/Map.css"
+import MapChart from "./components/Map/Map";
+
 
 const App = () => {
-  return (
+    const [content, setContent] = useState("");
+    return (
     <div>
-      <NavBar />
+        {/* <NavBar />
       <Switch>
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
         <Route component={NotFound}/>
-      </Switch>
+      </Switch> */}
+        <MapChart setTooltipContent={setContent} />
+        <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
