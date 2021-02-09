efe4cf50bad63bad4c56ba14005f46863175348f
Riad Becirevic <rbecirevic@ufl.edu>
2021-02-08 20:40:48 -0500

    Grind Session w/Luke
    Map, API, and login verified

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import {Route, Switch, Redirect} from 'react-router-dom';
 import Home from "./views/Home/Home";
 import NotFound from "./views/NotFound";
@@ -6,12 +6,16 @@ import NavBar from "./components/Header/NavBar";
 import SignUp from "./components/Authentication/Signup";
 import Login from "./components/Authentication/Login";
 import ForgotPassword from "./components/Authentication/ForgotPassword";
+import ReactTooltip from "react-tooltip";
+import "./components/Map/Map.css"
+import MapChart from "./components/Map/Map";
 
 const App = () => {
+    const [content, setContent] = useState("");
     return (
         <div>
             <NavBar/>
-            <Switch>
+           {/* <Switch>
                 <Route exact path="/Home" component={Home}/>
                 <Route exact path="/Signup" component={SignUp}/>
                 <Route exact path="/Login" component={Login}/>
@@ -20,7 +24,9 @@ const App = () => {
                     <Redirect to="/Home"/>
                 </Route>
                 <Route component={NotFound}/>
-            </Switch>
+            </Switch>*/}
+            <MapChart setTooltipContent={setContent} />
+            <ReactTooltip>{content}</ReactTooltip>
         </div>
     );
 }
